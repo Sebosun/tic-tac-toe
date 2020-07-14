32c8983678e55739cc1e92235ddfbcb22b1a836e
Sebosu <sebos@protonmail.com>
2020-07-14 16:05:56 +0200

    nothing substantial, pushing for git so i can access it on other pc lmao

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const ticTacSelector = document.getElementsByClassName('grid-container');
 
+// player Factory Function
+const playerFactory = (name, marker) =>{
+    return (name, marker)
+}
+
+const playerOne = playerFactory('one', 'X');
+const playerTwo = playerFactory('two', 'O');
+
+//array obiektów? może [[][][][][][]][][][][]] etc. w każdym storowany jest player.marker hmmmm
