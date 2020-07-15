3c0694980933b687e67a7b5e11cc957e463bc14a
Sebosun <sebos@protonmail.com>
2020-07-15 14:45:06 +0200

    putting functions into a module, and added eventListener for grids

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.createElement('div');
+let gridElems;
+
 container.classList.add('grid-container');
 
 // gameBoard which will be later used by a function to genererate it
@@ -7,38 +9,80 @@ let gameBoard = {
     1: '',
     2: '',
     3: '',
-    4: 'Dupa',
+    4: '',
     5: '',
     6: '',
     7: '',
     8: '',
 }
 
-let generateGrid = (board) => {
-    container.innerHTML = ''; // removes the board so it can be regenerated
-    for (let i = 0; i < 9; i++) {
-        let div = document.createElement('div');
-        div.classList.add('grid-element');
-        div.setAttribute('id', `grid-${i}`);
-        div.textContent = board[i]
-        container.appendChild(div);
-    }    
-}
+// generate module for my single use stuff
+
+const game = (() => {
+    const generateGrid = (board) => {
+        container.innerHTML = ''; // removes the board so it can be regenerated
+        for (let i = 0; i < 9; i++) {
+            let div = document.createElement('div');
+            div.classList.add('grid-element');
+            div.setAttribute('id', i);
+            div.textContent = board[i]
+            container.appendChild(div);
+        }    
+        // query Selector which will be used lateron for a listener
+        gridElems = document.querySelectorAll('.grid-element');
+        changeGridValue();
+    };
+
+    const changeGridValue = () => {
+        gridElems.forEach((place) =>{
+            place.addEventListener('click', (e) => {
+                gameBoard[e.target.id] = 'O';
+                generateGrid(gameBoard);
+            });
+        });
+    };
+
+    return{
+        generateGrid,
+        changeGridValue,
+    };
+})();
 
 
-
-// takes in object and fills the text content according to the object
-
+// generates the grid with the board object provided
+// let generateGrid = (board) => {
+//     container.innerHTML = ''; // removes the board so it can be regenerated
+//     for (let i = 0; i < 9; i++) {
+//         let div = document.createElement('div');
+//         div.classList.add('grid-element');
+//         div.setAttribute('id', i);
+//         div.textContent = board[i]
+//         container.appendChild(div);
+//     }    
+//     // query Selector which will be used lateron for a listener
+//     gridElems = document.querySelectorAll('.grid-element');
+// }
 
 document.body.appendChild(container);
 
+// function changeGridValue(){
+//     gridElems.forEach((place) =>{
+//         place.addEventListener('click', (e) => {
+//             console.log(e.target.id);
+            
+//         });
+//     })
+// }
+
 // player Factory Function
-const playerFactory = (name, marker) =>{
-    return (name, marker)
-}
+// const playerFactory = (name, marker) =>{
+//     return (name, marker)
+// }
 
-const playerOne = playerFactory('one', 'X');
-const playerTwo = playerFactory('two', 'O');
-//array obiektów? może [[][][][][][]][][][][]] etc. w każdym storowany jest player.marker hmmmm
+//const playerOne = playerFactory('one', 'X');
+//const playerTwo = playerFactory('two', 'O');
 
-generateGrid(gameBoard);
+game.generateGrid(gameBoard);
+game.changeGridValue();
+
+console.log(gridElems);
